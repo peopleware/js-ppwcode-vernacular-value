3ad3f7013540289e0161fb5e6d2aed13dc296967
Jan Dockx <Jan_Dockx@peopleware.be>
2015-02-26 14:15:54 +0100

    Code format.

--- TextComponentMassFraction.js
+++ TextComponentMassFraction.js
@@ -19,81 +19,81 @@ define(["dojo/_base/declare", "./MassFraction", "./_QualifiedValue",
   function(declare, MassFraction, _QualifiedValue,
            js, UndeterminedMassFraction, lang, module) {
 
-      var TextComponentMassFraction = declare([MassFraction, _QualifiedValue], {
+    var TextComponentMassFraction = declare([MassFraction, _QualifiedValue], {
 
-        _c_invar: [
-          function() {return this._c_prop_mandatory("componentName");},
-          function() {return this._c_prop_string("componentName");},
-          function() {return this._c_prop_mandatory("order");},
-          function() {return this._c_prop_number("order");}
-        ],
+      _c_invar: [
+        function() {return this._c_prop_mandatory("componentName");},
+        function() {return this._c_prop_string("componentName");},
+        function() {return this._c_prop_mandatory("order");},
+        function() {return this._c_prop_number("order");}
+      ],
 
-        // order: Number
-        order: null,
+      // order: Number
+      order: null,
 
-        // componentName: String
-        //   Textual representation of the component.
-        componentName: null,
+      // componentName: String
+      //   Textual representation of the component.
+      componentName: null,
 
-        constructor: function(/*Object*/ props) {
-          this._c_pre(function() {return props /* exists and not null */;});
-          this._c_pre(function() {return js.typeOf(props) === "object";});
-          this._c_pre(function() {return this._c_prop_mandatory(props, "scalarValue");});
-          this._c_pre(function() {return this._c_prop_number(props, "scalarValue");});
-          this._c_pre(function() {return this._c_prop_mandatory(props, "unit");});
-          this._c_pre(function() {return this._c_prop_string(props, "unit");});
-          this._c_pre(function() {return this.getSupportedUnits().contains(props.unit);});
-          this._c_pre(function() {return this._c_prop_mandatory(props, "componentName");});
-          this._c_pre(function() {return this._c_prop_string(props, "componentName");});
-          this._c_pre(function() {return this._c_prop_mandatory(props, "order");});
-          this._c_pre(function() {return this._c_prop_number(props, "order");});
+      constructor: function(/*Object*/ props) {
+        this._c_pre(function() {return props /* exists and not null */;});
+        this._c_pre(function() {return js.typeOf(props) === "object";});
+        this._c_pre(function() {return this._c_prop_mandatory(props, "scalarValue");});
+        this._c_pre(function() {return this._c_prop_number(props, "scalarValue");});
+        this._c_pre(function() {return this._c_prop_mandatory(props, "unit");});
+        this._c_pre(function() {return this._c_prop_string(props, "unit");});
+        this._c_pre(function() {return this.getSupportedUnits().contains(props.unit);});
+        this._c_pre(function() {return this._c_prop_mandatory(props, "componentName");});
+        this._c_pre(function() {return this._c_prop_string(props, "componentName");});
+        this._c_pre(function() {return this._c_prop_mandatory(props, "order");});
+        this._c_pre(function() {return this._c_prop_number(props, "order");});
 
-          this.componentName = props.componentName;
-          this.order = props.order;
-        },
+        this.componentName = props.componentName;
+        this.order = props.order;
+      },
 
-        equals: function(/*ScalarQuantitativeValue*/ other) {
-          return this.inherited(arguments) && this.componentName === other.componentName;
-          // we don't take the order into account
-        },
+      equals: function(/*ScalarQuantitativeValue*/ other) {
+        return this.inherited(arguments) && this.componentName === other.componentName;
+        // we don't take the order into account
+      },
 
-        getQualifier: function() {
-          return this.componentName;
-        },
+      getQualifier: function() {
+        return this.componentName;
+      },
 
-        add: function(/*Fraction*/ other) {
-          var sum = this.scalarValue + other.getScalarValueAs(this.unit);
-          if (this.component = other.component) {
-            return new (this.constructor)({
-              scalarValue: sum,
-              unit: this.unit,
-              componentName: this.componentName,
-              order: Math.min(this.order, other.order)
-            });
-          }
-          else {
-            return new UndeterminedMassFraction({scalarValue: sum, unit: this.unit});
-          }
-        },
-
-        _extendJsonObject: function(/*Object*/ json) {
-          json.componentName = this.componentName;
-          json.order = this.order;
-        },
-
-        _stateToString: function(/*Array of String*/ toStrings) {
-          toStrings.push("componentName: " + this.componentName);
-          toStrings.push("order: " + this.order);
+      add: function(/*Fraction*/ other) {
+        var sum = this.scalarValue + other.getScalarValueAs(this.unit);
+        if (this.component == other.component) {
+          return new (this.constructor)({
+            scalarValue: sum,
+            unit: this.unit,
+            componentName: this.componentName,
+            order: Math.min(this.order, other.order)
+          });
         }
+        else {
+          return new UndeterminedMassFraction({scalarValue: sum, unit: this.unit});
+        }
+      },
 
-      });
+      _extendJsonObject: function(/*Object*/ json) {
+        json.componentName = this.componentName;
+        json.order = this.order;
+      },
 
-      TextComponentMassFraction.format = MassFraction.format;
-      TextComponentMassFraction.parse = lang.partial(MassFraction.parse, TextComponentMassFraction);
+      _stateToString: function(/*Array of String*/ toStrings) {
+        toStrings.push("componentName: " + this.componentName);
+        toStrings.push("order: " + this.order);
+      }
+
+    });
+
+    TextComponentMassFraction.format = MassFraction.format;
+    TextComponentMassFraction.parse = lang.partial(MassFraction.parse, TextComponentMassFraction);
     // MUDO componentName, order
 
-      TextComponentMassFraction.mid = module.id;
+    TextComponentMassFraction.mid = module.id;
 
-      return TextComponentMassFraction;
-    }
+    return TextComponentMassFraction;
+  }
 );
